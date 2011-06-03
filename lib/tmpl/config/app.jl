66cc745ab5e0104c23c0fd8eb90dc803ae2c9043
Drew Young <drewyoung1@gmail.com>
2011-06-03 17:29:28 -0400

    Added running the environment file

--- lib/tmpl/config/app.jl
+++ lib/tmpl/config/app.jl
@@ -5,3 +5,11 @@ exports.hi = 'hi';
 exports.newClass = new Class({
 	hi: 'drew'
 });
+
+console.log('env: ', env);
+
+env.hi = 'hi'
+
+hi = 'hi'
+
+person = 'safsa'
--- lib/tmpl/config/env.jl
+++ lib/tmpl/config/env.jl
@@ -1,1 +1,1 @@
-
+console.log('env.jl')
